39bb76f157fb914d6162ec55f7d25ff702dc47b5
Vladislav Smirnov <it.smirnov.vladislav@yandex.ru>
2023-06-13 22:33:21 +0300

    Update ChipModel.kt

--- app/chips/src/main/java/com/sgcdeveloper/chips/model/chips/ChipModel.kt
+++ app/chips/src/main/java/com/sgcdeveloper/chips/model/chips/ChipModel.kt
@@ -1,11 +1,11 @@
 package com.sgcdeveloper.chips.model.chips
 
-abstract class ChipModel(val id: String = "", val isEnable: Boolean) {
-    abstract fun <T : ChipModel> copy(id: String = this.id, isEnable: Boolean = this.isEnable): T
+abstract class ChipModel(val id: String = "", val isEnabled: Boolean) {
+    abstract fun <T : ChipModel> copy(id: String = this.id, isEnabled: Boolean = this.isEnabled): T
 
     override fun equals(other: Any?): Boolean {
         if (other is ChipModel) {
-            return other.id == id && other.isEnable == isEnable
+            return other.id == id && other.isEnabled == isEnabled
         }
         return false
     }
